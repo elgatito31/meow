42dd3316b76149177b893877d68b71494459907a
elgatito31 <104541053+elgatito31@users.noreply.github.com>
2024-02-08 12:47:56 -0500

    meow this is locks and semaphores

--- elgaton.c
+++ elgaton.c
@@ -1,35 +1,65 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define NUM_THREADS 5
+#define BUFFER_SIZE 10
 
-void *thread_function(void *arg) {
-    int thread_id = *(int *)arg;
-    printf("Thread %d is running\n", thread_id);
-    pthread_exit(NULL);
+int buffer[BUFFER_SIZE]; // shared buffer
+int in = 0; // next place to produce
+int out = 0; // next place to consume
+
+sem_t empty; // number of empty spaces in buffer, initially BUFFER_SIZE
+sem_t full; // number of elements in buffer, initially 0
+pthread_mutex_t mutex; // ensures that buffer is accessed by at most one thread at a time
+
+void* producer(void* arg) {
+    for (int i = 0; i < 20; i++) {
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+
+        buffer[in] = i;
+        printf("Produced %d at %d\n", i, in);
+        in = (in + 1) % BUFFER_SIZE;
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&full);
+    }
+
+    return NULL;
+}
+
+void* consumer(void* arg) {
+    for (int i = 0; i < 20; i++) {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+
+        int item = buffer[out];
+        printf("Consumed %d from %d\n", item, out);
+        out = (out + 1) % BUFFER_SIZE;
+
+        pthread_mutex_unlock(&mutex);
+        sem_post(&empty);
+    }
+
+    return NULL;
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    int thread_args[NUM_THREADS];
-    int i;
+    pthread_t producer_thread, consumer_thread;
 
-    for (i = 0; i < NUM_THREADS; i++) {
-        thread_args[i] = i;
-        if (pthread_create(&threads[i], NULL, thread_function, &thread_args[i]) != 0) {
-            fprintf(stderr, "Error creating thread %d\n", i);
-            exit(1);
-        }
-    }
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
+    pthread_mutex_init(&mutex, NULL);
 
-    for (i = 0; i < NUM_THREADS; i++) {
-        if (pthread_join(threads[i], NULL) != 0) {
-            fprintf(stderr, "Error joining thread %d\n", i);
-            exit(1);
-        }
-    }
+    pthread_create(&producer_thread, NULL, producer, NULL);
+    pthread_create(&consumer_thread, NULL, consumer, NULL);
 
-    printf("All threads have completed\n");
+    pthread_join(producer_thread, NULL);
+    pthread_join(consumer_thread, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    pthread_mutex_destroy(&mutex);
+
     return 0;
 }
