6c3a9af2c4204aff0eee9b64754d794dbe68210a
elgatito31 <104541053+elgatito31@users.noreply.github.com>
2024-02-13 11:12:41 -0500

    meow in class

--- elgaton.c
+++ elgaton.c
@@ -1,65 +1,31 @@
 #include <stdio.h>
 #include <pthread.h>
-#include <semaphore.h>
 
-#define BUFFER_SIZE 10
-
-int buffer[BUFFER_SIZE]; // shared buffer
-int in = 0; // next place to produce
-int out = 0; // next place to consume
-
-sem_t empty; // number of empty spaces in buffer, initially BUFFER_SIZE
-sem_t full; // number of elements in buffer, initially 0
-pthread_mutex_t mutex; // ensures that buffer is accessed by at most one thread at a time
-
-void* producer(void* arg) {
-    for (int i = 0; i < 20; i++) {
-        sem_wait(&empty);
-        pthread_mutex_lock(&mutex);
-
-        buffer[in] = i;
-        printf("Produced %d at %d\n", i, in);
-        in = (in + 1) % BUFFER_SIZE;
-
-        pthread_mutex_unlock(&mutex);
-        sem_post(&full);
-    }
-
-    return NULL;
+void *print_even(void *arg) {
+  for (int i = 2; i <= 10; i += 2) {
+    printf("Even: %d\n", i);
+  }
+  return NULL;
 }
 
-void* consumer(void* arg) {
-    for (int i = 0; i < 20; i++) {
-        sem_wait(&full);
-        pthread_mutex_lock(&mutex);
-
-        int item = buffer[out];
-        printf("Consumed %d from %d\n", item, out);
-        out = (out + 1) % BUFFER_SIZE;
-
-        pthread_mutex_unlock(&mutex);
-        sem_post(&empty);
-    }
-
-    return NULL;
+void *print_odd(void *arg) {
+  for (int i = 1; i <= 9; i += 2) {
+    printf("Odd: %d\n", i);
+  }
+  return NULL;
 }
 
 int main() {
-    pthread_t producer_thread, consumer_thread;
+  pthread_t thread_even, thread_odd;
 
-    sem_init(&empty, 0, BUFFER_SIZE);
-    sem_init(&full, 0, 0);
-    pthread_mutex_init(&mutex, NULL);
+  // Create the threads
+  pthread_create(&thread_even, NULL, print_even, NULL);
+  pthread_create(&thread_odd, NULL, print_odd, NULL);
 
-    pthread_create(&producer_thread, NULL, producer, NULL);
-    pthread_create(&consumer_thread, NULL, consumer, NULL);
+  // Wait for the threads to finish
+  pthread_join(thread_even, NULL);
+  pthread_join(thread_odd, NULL);
 
-    pthread_join(producer_thread, NULL);
-    pthread_join(consumer_thread, NULL);
-
-    sem_destroy(&empty);
-    sem_destroy(&full);
-    pthread_mutex_destroy(&mutex);
-
-    return 0;
+  printf("All threads finished!\n");
+  return 0;
 }
